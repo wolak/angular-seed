0bcf0ad17a4bf52d64e6bd360e2669542674d7d6
wolakpaul <wolakpaul@gmail.com>
2014-03-22 15:52:18 -0400

    First navigation menu

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,9 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/bitties', {templateUrl: 'partials/bitties.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/sports', {templateUrl: 'partials/sports.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/notes', {templateUrl: 'partials/notes.html', controller: 'MyCtrl2'});
+  //$routeProvider.otherwise({redirectTo: '/view1'});
 }]);
