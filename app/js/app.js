ed73dbb41ce33957f29a5c0a8b87f5daedc1bcc2
wolakpaul <wolakpaul@gmail.com>
2014-03-24 19:33:47 -0400

    Why doesnt this work?

--- app/js/app.js
+++ app/js/app.js
@@ -11,8 +11,10 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/bitties', {templateUrl: 'partials/bitties.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/sports', {templateUrl: 'partials/sports.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/notes', {templateUrl: 'partials/notes.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/bitties', {templateUrl: 'partials/bitties.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/sports', {templateUrl: 'partials/sports.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/notes', {templateUrl: 'partials/notes.html', controller: 'MyCtrl1'});
   //$routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,23 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-	controller('MyCtrl1', [function() {
-		
-	}])
-	.controller('MyCtrl2', [function() {
-
+	controller('MyCtrl1', ['$scope', function($scope){
+		$scope.menus = [{
+				'class': "test",
+				'location': "tester",
+				'label': "TEST!"
+			}
+		];
+		 $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 2},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 3}
+  ];
+		console.log("hello", $scope, $scope.menus);
 	}]);
